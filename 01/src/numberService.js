53a72610c389f0a47311e11588d623786475765d
Tim Kruse <coding@tim-kruse.com>
2023-12-01 13:07:54 +0100

    Added 01

--- 01/src/numberService.js
+++ 01/src/numberService.js
@@ -1,43 +1,43 @@
-function getFirstNumber(value) {
-    let currentVal = ''
-    for (const char of value) {
-        currentVal += char
-        if (!isNaN(char)) return char
-        let converted = convertStringNumber(currentVal);
-        if (converted.length !== 0 && !isNaN(converted[0])) return converted
-    }
-    return 0
+function getFirstNumber (value) {
+  let currentVal = ''
+  for (const char of value) {
+    currentVal += char
+    if (!isNaN(char)) return char
+    const converted = convertStringNumber(currentVal)
+    if (converted.length !== 0 && !isNaN(converted[0])) return converted
+  }
+  return 0
 }
 
-function getLastNumber(value) {
-    let currentVal = ''
-    for (let i = value.length - 1; i >= 0; i--) {
-        const char = value.charAt(i)
-        currentVal = char + currentVal
-        if (!isNaN(char)) return char
-        let converted = convertStringNumber(currentVal);
-        if (converted.length !== 0 && !isNaN(converted[0])) return converted[0]
-    }
-    return 0
+function getLastNumber (value) {
+  let currentVal = ''
+  for (let i = value.length - 1; i >= 0; i--) {
+    const char = value.charAt(i)
+    currentVal = char + currentVal
+    if (!isNaN(char)) return char
+    const converted = convertStringNumber(currentVal)
+    if (converted.length !== 0 && !isNaN(converted[0])) return converted[0]
+  }
+  return 0
 }
 
-export function getCombinedNumber(value) {
-    const firstNumber = getFirstNumber(value)
-    const lastNumber = getLastNumber(value)
-    return firstNumber + '' + lastNumber
+export function getCombinedNumber (value) {
+  const firstNumber = getFirstNumber(value)
+  const lastNumber = getLastNumber(value)
+  return +(firstNumber + '' + lastNumber)
 }
 
-function convertStringNumber(value) {
-    return [
-        {label: 'one', number: 1},
-        {label: 'two', number: 2},
-        {label: 'three', number: 3},
-        {label: 'four', number: 4},
-        {label: 'five', number: 5},
-        {label: 'six', number: 6},
-        {label: 'seven', number: 7},
-        {label: 'eight', number: 8},
-        {label: 'nine', number: 9}]
-        .filter(n => value.includes(n.label))
-        .map(v => v.number)
+function convertStringNumber (value) {
+  return [
+    { label: 'one', number: 1 },
+    { label: 'two', number: 2 },
+    { label: 'three', number: 3 },
+    { label: 'four', number: 4 },
+    { label: 'five', number: 5 },
+    { label: 'six', number: 6 },
+    { label: 'seven', number: 7 },
+    { label: 'eight', number: 8 },
+    { label: 'nine', number: 9 }]
+    .filter(n => value.includes(n.label))
+    .map(v => v.number)
 }
--- 01/src/numberService.test.js
+++ 01/src/numberService.test.js
@@ -1,37 +1,53 @@
 import { expect, test } from 'vitest'
-import {getCombinedNumber} from "./numberService.js";
-
-
-
-/*console.log('12', getCombinedNumber('1abc2'))
-console.log('38', getCombinedNumber('pqr3stu8vwx'))
-console.log('15', getCombinedNumber('a1b2c3d4e5f'))
-console.log('77', getCombinedNumber('treb7uchet'))
-
-console.log('29', getCombinedNumber('two1nine'))
-console.log('14', getCombinedNumber('zoneight234'))
-console.log('13', getCombinedNumber('abcone2threexyz'))
-console.log('24', getCombinedNumber('xtwone3four'))
-console.log('42', getCombinedNumber('4nineeightseven2'))
-console.log('76', getCombinedNumber('7pqrstsixteen'))
-console.log('83', getCombinedNumber('eightwothree'))
-console.log('83', getCombinedNumber('eighthree'))
-console.log('79', getCombinedNumber('sevenine'))*/
-
-
+import { getCombinedNumber } from './numberService.js'
 
 test('Input "1abc2" converts to 12', () => {
-    expect(getCombinedNumber('1abc2')).toBe('12')
+  expect(getCombinedNumber('1abc2')).toBe(12)
 })
 
 test('Input "pqr3stu8vwx" converts to 38', () => {
-    expect(getCombinedNumber('pqr3stu8vwx')).toBe('38')
+  expect(getCombinedNumber('pqr3stu8vwx')).toBe(38)
 })
 
 test('Input "a1b2c3d4e5f" converts to 15', () => {
-    expect(getCombinedNumber('a1b2c3d4e5f')).toBe('15')
+  expect(getCombinedNumber('a1b2c3d4e5f')).toBe(15)
 })
 
 test('Input "treb7uchet" converts to 77', () => {
-    expect(getCombinedNumber('treb7uchet')).toBe('77')
+  expect(getCombinedNumber('treb7uchet')).toBe(77)
+})
+
+test('Input "xtwone3four" converts to 24', () => {
+  expect(getCombinedNumber('xtwone3four')).toBe(24)
+})
+
+test('Input "two1nine" converts to 29', () => {
+  expect(getCombinedNumber('two1nine')).toBe(29)
+})
+
+test('Input "abcone2threexyz" converts to 13', () => {
+  expect(getCombinedNumber('abcone2threexyz')).toBe(13)
+})
+
+test('Input "zoneight234" converts to 14', () => {
+  expect(getCombinedNumber('zoneight234')).toBe(14)
+})
+
+test('Input "4nineeightseven2" converts to 42', () => {
+  expect(getCombinedNumber('4nineeightseven2')).toBe(42)
+})
+
+test('Input "7pqrstsixteen" converts to 76', () => {
+  expect(getCombinedNumber('7pqrstsixteen')).toBe(76)
+})
+
+test('Input "eightwothree" converts to 83', () => {
+  expect(getCombinedNumber('eightwothree')).toBe(83)
+})
+test('Input "eighthree" converts to 83', () => {
+  expect(getCombinedNumber('eighthree')).toBe(83)
+})
+
+test('Input "sevenine" converts to 79', () => {
+  expect(getCombinedNumber('sevenine')).toBe(79)
 })
