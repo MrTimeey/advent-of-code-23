efeb5146e536df340b6fb386d9ede132bc52f702
Tim Kruse <coding@tim-kruse.com>
2023-12-03 18:08:22 +0100

    Added java template

---
+++ project_templates/java/src/main/java/io/github/mrtimeey/aoc/Main.java
@@ -0,0 +1,27 @@
+package io.github.mrtimeey.aoc;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+   public static void main(String[] args) {
+      List<String> input = loadInput();
+      System.out.println("Hello World!" + input);
+   }
+
+   private static List<String> loadInput() {
+      URL resource = Main.class.getResource("/input.txt");
+      if (resource == null) return List.of();
+      try (Stream<String> stream = Files.lines(Path.of(resource.toURI()))) {
+         return stream.toList();
+      } catch (IOException | URISyntaxException e) {
+         return List.of();
+      }
+   }
+}
