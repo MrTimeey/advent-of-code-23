86be33d3b395fc1c5af44e9eac8f8327fa0fab6c
Tim Kruse <coding@tim-kruse.com>
2023-12-05 21:14:34 +0100

    Prepare day 6

---
+++ 06-java/src/test/java/io/github/mrtimeey/aoc/MainTest.java
@@ -0,0 +1,15 @@
+package io.github.mrtimeey.aoc;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+
+   @Test
+   void test() {
+      assertThat(2 * 2).isEqualTo(4);
+   }
+
+}
