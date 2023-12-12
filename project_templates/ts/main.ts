6902bbc77504554a333950d8487d35181d879142
Tim Kruse <coding@tim-kruse.com>
2023-12-12 08:44:07 +0100

    template changed

--- project_templates/ts/main.ts
+++ project_templates/ts/main.ts
@@ -1,8 +1,10 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from 'fs'
 
 const readFileLines = (filename: string): string[] =>
-    readFileSync(filename).toString('utf-8').split('\n');
+    readFileSync(filename).toString('utf-8').trimEnd().split('\n').map(line => line.trimEnd())
 
-const inputLines = readFileLines('./src/input.txt');
+const inputLines = readFileLines('./src/input.txt')
 
 console.log('XX.12.2023')
+console.log('Part 1: ')
+console.log('Part 2: ')
