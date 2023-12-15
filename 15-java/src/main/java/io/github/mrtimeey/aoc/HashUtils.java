e45feea3bf0e40e50bf99d3ffca906f4ea9524ff
Tim Kruse <coding@tim-kruse.com>
2023-12-15 12:38:35 +0100

    refactored

--- 15-java/src/main/java/io/github/mrtimeey/aoc/HashUtils.java
+++ 15-java/src/main/java/io/github/mrtimeey/aoc/HashUtils.java
@@ -22,16 +22,14 @@ public class HashUtils {
       for (String input : inputStrings.split(",")) {
          if (input.contains("-")) {
             String toRemove = input.replace("-", "");
-            boxes.get(hashValue(toRemove)).removeIf(l -> l.getName().equals(toRemove));
+            boxes.get(hashValue(toRemove)).removeIf(l -> l.name().equals(toRemove));
          }
          if (input.contains("=")) {
             String[] split = input.split("=");
             Lens newLens = Lens.of(split[0], Integer.parseInt(split[1]));
-            int boxIndex = hashValue(newLens.getName());
-            if (boxes.get(boxIndex).stream().anyMatch(l -> l.getName().equals(newLens.getName()))) {
-               boxes.get(boxIndex).stream()
-                     .filter(l -> l.getName().equals(newLens.getName()))
-                     .forEach(l -> l.setFocalLength(newLens.getFocalLength()));
+            int boxIndex = hashValue(newLens.name());
+            if (boxes.get(boxIndex).stream().anyMatch(l -> l.name().equals(newLens.name()))) {
+               boxes.get(boxIndex).replaceAll(lens -> lens.name().equals(newLens.name()) ? newLens : lens);
             } else {
                boxes.get(boxIndex).add(newLens);
             }
@@ -40,7 +38,7 @@ public class HashUtils {
       int total = 0;
       for (int boxIndex = 0; boxIndex < boxes.size(); boxIndex++) {
          for (int slotIndex = 0; slotIndex < boxes.get(boxIndex).size(); slotIndex++) {
-            total += (boxIndex+1) * (slotIndex + 1) * boxes.get(boxIndex).get(slotIndex).getFocalLength();
+            total += (boxIndex+1) * (slotIndex + 1) * boxes.get(boxIndex).get(slotIndex).focalLength();
          }
       }
       return total;
@@ -49,11 +47,17 @@ public class HashUtils {
    public static int hashValue(String input) {
       int currentValue = 0;
       for (char c : input.toCharArray()) {
-         currentValue += (int) c;
+         currentValue += c;
          currentValue = currentValue * 17;
          currentValue = currentValue % 256;
       }
       return currentValue;
+   }
+
+   record Lens(String name, int focalLength) {
+      static Lens of(String name, int focalLength) {
+         return new Lens(name, focalLength);
+      }
    }
 
 }
