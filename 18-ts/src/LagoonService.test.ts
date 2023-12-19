3656153987e2aa2186fe48297680f5fd6e45fb49
Tim Kruse <coding@tim-kruse.com>
2023-12-19 20:49:52 +0100

    day 18

--- 18-ts/src/LagoonService.test.ts
+++ 18-ts/src/LagoonService.test.ts
@@ -1,25 +1,29 @@
 import { expect, test } from 'vitest'
-import { part1 } from './LagoonService'
+import { part1, part2 } from './LagoonService'
+
+const exampleInput = [
+  'R 6 (#70c710)',
+  'D 5 (#0dc571)',
+  'L 2 (#5713f0)',
+  'D 2 (#d2c081)',
+  'R 2 (#59c680)',
+  'D 2 (#411b91)',
+  'L 5 (#8ceee2)',
+  'U 2 (#caa173)',
+  'L 1 (#1b58a2)',
+  'U 2 (#caa171)',
+  'R 2 (#7807d2)',
+  'U 3 (#a77fa3)',
+  'L 2 (#015232)',
+  'U 2 (#7a21e3)'
+]
 
 test('Part 1', () => {
-  const input = [
-    'R 6 (#70c710)',
-    'D 5 (#0dc571)',
-    'L 2 (#5713f0)',
-    'D 2 (#d2c081)',
-    'R 2 (#59c680)',
-    'D 2 (#411b91)',
-    'L 5 (#8ceee2)',
-    'U 2 (#caa173)',
-    'L 1 (#1b58a2)',
-    'U 2 (#caa171)',
-    'R 2 (#7807d2)',
-    'U 3 (#a77fa3)',
-    'L 2 (#015232)',
-    'U 2 (#7a21e3)'
-  ]
-
-  const result = part1(input)
-
+  const result = part1(exampleInput)
   expect(result).toBe(62)
+})
+
+test('Part 2', () => {
+  const result = part2(exampleInput)
+  expect(result).toBe(952408144115)
 })
--- 18-ts/src/LagoonService.ts
+++ 18-ts/src/LagoonService.ts
@@ -1,7 +1,75 @@
 export const part1 = (inputLines: string[]): number => {
-  return 0
+  const input: InputEntry[] = parseInput(inputLines)
+  const radiusLength = input.map((e: InputEntry) => e.length).reduce((acc, val) => acc + val, 0)
+  const coordinates = getCoordinates(input)
+  return calculateArea(coordinates, radiusLength)
 }
 
 export const part2 = (inputLines: string[]): number => {
-  return 0
+  let input: InputEntry[] = parseInput(inputLines)
+  input = fixInput(input)
+  const radiusLength = input.map((e: InputEntry) => e.length).reduce((acc, val) => acc + val, 0)
+  const coordinates = getCoordinates(input)
+  return calculateArea(coordinates, radiusLength)
+}
+
+interface InputEntry {
+  direction: 'R' | 'D' | 'L' | 'U'
+  length: number
+  color: string
+}
+
+const directionMapping = {
+  R: [1, 0],
+  L: [-1, 0],
+  U: [0, 1],
+  D: [0, -1]
+}
+
+const mapDirection = (direction: string): 'R' | 'D' | 'L' | 'U' => {
+  if (direction === 'R') return 'R'
+  if (direction === 'D') return 'D'
+  if (direction === 'L') return 'L'
+  return 'U'
+}
+
+const parseInput = (inputLines: string[]): InputEntry[] => {
+  return inputLines.map(row => {
+    const [direction, length, color] = row.split(' ')
+    return { direction: mapDirection(direction), length: +length, color }
+  })
+}
+
+const fixInput = (input: InputEntry[]): InputEntry[] => {
+  return input
+    .map(i => {
+      const dirs = ['R', 'D', 'L', 'U']
+      const length = parseInt(i.color.substring(2, 7), 16)
+      const directionNumber = parseInt(i.color.substring(7, 8))
+      const direction = mapDirection(dirs[directionNumber])
+      return { direction, length, color: i.color }
+    })
+}
+
+const getCoordinates = (input: InputEntry[]): number[][] => {
+  const result = [[0, 0]]
+  input.forEach(e => {
+    const [currentX, currentY] = result[result.length - 1]
+    const [directionX, directionY] = directionMapping[e.direction]
+    result.push([currentX + directionX * e.length, currentY + directionY * e.length])
+  })
+  return result
+}
+
+const calculateArea = (coordinates: number[][], radiusLength: number): number => {
+  // Shoelace formula
+  let sum = 0
+  for (let i = 0; i < coordinates.length; i++) {
+    const next = coordinates[i + 1] ? coordinates[i + 1] : coordinates[0]
+    const [x, y] = coordinates[i]
+    const [nx, ny] = next
+    sum += x * ny - y * nx
+  }
+  const shoelace = Math.abs(sum / 2)
+  return shoelace + radiusLength / 2 + 1
 }
